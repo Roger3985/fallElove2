fb8195c497b6ae8f01c8a0f944b1f819ca47eb89
Leon <firesnoopy0731@gmail.com>
2024-04-04 06:51:56 +0800

    場地VO 已更新型態

---
+++ fallElove_springboot/src/main/java/JavaBean/studio/StudioInfo.java
@@ -0,0 +1,38 @@
+package JavaBean.studio;
+
+import java.math.BigDecimal;
+
+public class StudioInfo {
+	private Integer sNo;
+	private String sName;
+	private String sInfo;
+	private BigDecimal sPrice;
+	
+	public Integer getsNo() {
+		return sNo;
+	}
+	public void setsNo(Integer sNo) {
+		this.sNo = sNo;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsInfo() {
+		return sInfo;
+	}
+	public void setsInfo(String sInfo) {
+		this.sInfo = sInfo;
+	}
+	public BigDecimal getsPrice() {
+		return sPrice;
+	}
+	public void setsPrice(BigDecimal sPrice) {
+		this.sPrice = sPrice;
+	}
+	
+
+
+}
